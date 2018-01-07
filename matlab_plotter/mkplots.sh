e1dc848fc5e7374f1ae45c676a1e2b28f872d415
D.B <thejan.2009@gmail.com>
2018-01-07 09:15:21 +0100

    fix

--- matlab_plotter/mkplots.sh
+++ matlab_plotter/mkplots.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-# folder=$1
-folder=../coupled-ab/2018-01-06_14-26-45
+folder=$1
+# folder=../coupled-ab/2018-01-06_14-26-45
 
-for i in $folder/*; do
-    echo $i;
-    a=${i##*/};
-    a=${a#alpha-};
-    echo "alpha ${a%-beta*}" > $i/results
-    echo "beta ${a#*beta-}" >> $i/results
-    octave p.m $i/lacI_ALL.csv $i/lacl lacI >> $i/results
+for j in $folder/*; do
+    for i in $j/*; do
+        echo $i;
+        a=${i##*/};
+        a=${a#alpha-};
+        echo "alpha ${a%-beta*}" > $i/results
+        echo "beta ${a#*beta-}" >> $i/results
+        octave p.m $i/lacI_ALL.csv $i/lacl lacI >> $i/results
+    done
 done
