dd58be85439248e1acee4b877490910f68083dce
Robert Cvitkovič <robert.cvitkovic@gmail.com>
2018-01-11 22:18:35 +0100

    Add figuresize, mashe subplot and figure save

--- matlab_plotter/heatmap_plot.py
+++ matlab_plotter/heatmap_plot.py
@@ -5,10 +5,10 @@ from matplotlib.colors import LogNorm
 
 df = pd.read_csv("heyy.csv")
 
-value = 'period'
-#value = 'amplitude'
+#value = 'period'
+value = 'amplitude'
 
-show_pop = [100, 200, 500, 1000]
+show_pop = [20, 50, 100, 200, 500, 1000]
 
 # parse data
 data = [
@@ -22,12 +22,12 @@ data = [
 vmin = min(np.min(d.values) for d in data)
 vmax = max(np.max(d.values) for d in data)
 
-fig, axs = plt.subplots(1, len(show_pop))
+fig, axs = plt.subplots(2, int(len(show_pop)/2), figsize=(20, 12))
 
-for ax, d, p in zip(axs, data, show_pop):
+for ax, d, p in zip(axs.flatten(), data, show_pop):
     d = d[d.columns[::-1]]
-    print(d)
-    print(d.values)
+    #print(d)
+    #print(d.values)
     X, Y = np.meshgrid(d.index, d.columns)
     if value == 'period':
         im = ax.pcolormesh(X, Y, d.values, vmin=vmin, vmax=vmax)
@@ -39,8 +39,8 @@ for ax, d, p in zip(axs, data, show_pop):
     ax.set_xscale('log')
     ax.set_yscale('log')
 
-fig.subplots_adjust(right=0.8)
-cbar_ax = fig.add_axes([0.85, 0.29, 0.05, 0.41])
+fig.subplots_adjust(right=0.85)
+cbar_ax = fig.add_axes([0.87, 0.29, 0.05, 0.41])
 fig.colorbar(im, cax=cbar_ax)
 
-plt.show()
+plt.savefig(value + ".eps", bbox_inches='tight')
